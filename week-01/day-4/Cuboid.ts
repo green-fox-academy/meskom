6c5450550cc51770804a53e33bb3bf76516a4b43
Meskom <mihaly.mesko@gmail.com>
2019-04-05 11:00:41 +0200

    feat: add Cubiod exercise

--- week-01/day-4/Cuboid.ts
+++ week-01/day-4/Cuboid.ts
@@ -1,7 +1,15 @@
-use strict';
+
+'use strict';
+
 
 // Write a program that stores 3 sides of a cuboid as variables (floats)
 // The program should write the surface area and volume of the cuboid like:
 //
 // Surface Area: 600
 // Volume: 1000
+let a:number=10;
+let b:number=15;
+let c:number=10;
+console.log ('Surface Area: '+ 2*(a*b+a*c+ b*c));
+console.log ('Volume: '+ a*b*c);
+
