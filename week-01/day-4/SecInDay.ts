5a7e7c0f810625798521f6abb1f84f9db5729ad6
Meskom <mihaly.mesko@gmail.com>
2019-04-05 13:42:47 +0200

    Second in a day exercises

--- week-01/day-4/SecInDay.ts
+++ week-01/day-4/SecInDay.ts
@@ -1,9 +1,54 @@
+
 'use strict';
 
-let currentHours: number = 14;
-let currentMinutes: number = 34;
-let currentSeconds: number = 42;
+let a: number = 24;
+let out: number = 0;
 
-// Write a program that prints the remaining seconds (as an integer) from a
-// day if the current time is represented by these variables
-console.log((24*60*60)-(currentHours*60*60+currentMinutes*60+currentSeconds));    
+// if a is even increment out by one
+if (a%2==0){
+console.log(out-1);
+}
+let b: number = 13;
+let out2: string = '';
+
+if (10<b<20) 
+{
+out2='Sweet'; 
+}
+if (b<10) 
+{
+out2='Less'; 
+}
+if (20<b) 
+{
+out2='More'; 
+}
+// if b is between 10 and 20 set out2 to "Sweet!"
+// if less than 10 set out2 to "Less!",
+// if more than 20 set out2 to "More!"
+
+console.log(out2);
+
+let c: number = 123;
+let credits: number = 100;
+let isBonus: boolean = false;
+
+// if credits are at least 50,
+// and is_bonus is false decrement c by 2
+// if credits are smaller than 50,
+// and is_bonus is false decrement c by 1
+// if is_bonus is true c should remain the same
+
+console.log(c);
+
+let d: number =  8;
+let time: number = 120;
+let out3: string = '';
+
+// if d is dividable by 4
+// and time is not more than 200
+// set out3 to "check"
+// if time is more than 200
+// set out3 to "Time out"
+// otherwise set out3 to "Run Forest Run!"
+
