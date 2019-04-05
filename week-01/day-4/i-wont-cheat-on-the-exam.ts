6e7a5205525d3ba1562443083835221af48f2e13
Meskom <mihaly.mesko@gmail.com>
2019-04-05 12:08:57 +0200

    add i-wont-cheat-on-the-exam exercise

---
+++ week-01/day-4/i-wont-cheat-on-the-exam.ts
@@ -0,0 +1,9 @@
+
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+let a:string=' I won\'t cheat in the exam!'
+for (let i:number=0; i< 99; i++) {
+    console.log(a);
+}
