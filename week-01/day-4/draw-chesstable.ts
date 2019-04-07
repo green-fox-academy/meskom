d8c1ce24b43c9712af19f386591e6c98a78d3aac
Meskom <mihaly.mesko@gmail.com>
2019-04-07 22:21:31 +0200

    add Draw-Chesstable exercise

---
+++ week-01/day-4/draw-chesstable.ts
@@ -0,0 +1,27 @@
+// Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % % 
+//  % % % %
+let lineCount: number= 8;
+let lineLength: number=8;
+let line: string = '';
+
+for (let i: number = 1; i <= lineCount; i++) 
+{
+    line = '';
+    
+ for (let j: number= 1; j <= lineLength; j++){
+    if (i%2 ==0 && j%2 == 0 || i%2 == 1 && j%2 == 1 ) {
+        line +=' ';}
+    else { line += 'x ';
+    } 
+    
+}
+console.log(line);
+}
