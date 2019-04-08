147faf926e320fb430cc029d702c77e21eaddf20
Meskom <mihaly.mesko@gmail.com>
2019-04-08 16:11:47 +0200

    add compare-lenght exercise

---
+++ week-02/day-01/compare-length.ts
@@ -0,0 +1,11 @@
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+let firstList = [1, 2, 3];
+let secondList = [4, 5, 6, 7]
+
+if (secondList.length > firstList.length) {
+    console.log('secondList has more lements than firstList')
+}
