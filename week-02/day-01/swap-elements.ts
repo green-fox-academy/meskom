cbb46796fe9a586521724ca38397de9a8a6ef397
Meskom <mihaly.mesko@gmail.com>
2019-04-08 16:35:25 +0200

    add swap-elemnts exercise

---
+++ week-02/day-01/swap-elements.ts
@@ -0,0 +1,18 @@
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc = ['Arthur', 'Boe', 'Chloe'];
+
+abc.splice(0, 0, abc[2]);
+abc.splice(2, 0, abc[1]);
+abc.splice(1, 1,);
+abc.splice(3, 1,);
+function printParams(a: any[]) {
+    a.forEach(function (item: any, index: any) {
+        console.log(item);
+    });
+}
+
+console.log(printParams(abc))
+
+
