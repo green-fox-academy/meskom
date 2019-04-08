f82e7cb1a47efdf0cfa001f418f66cc17a812090
Meskom <mihaly.mesko@gmail.com>
2019-04-08 16:14:19 +0200

    add PrintParams exercise

---
+++ week-02/day-01/print-params.ts
@@ -0,0 +1,10 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+//function printParams ([a]) {console.log a.forEach}
+let a=[1,1,1,1,2,2];
+function printParams (a: number[]) { a.forEach(function(item: any,index: any){
+    console.log(item);});}
+
+    console.log (printParams(a))
