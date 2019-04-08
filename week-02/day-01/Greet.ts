d7f5d202de8164beabe9ff764bcc280f82540bea
Meskom <mihaly.mesko@gmail.com>
2019-04-08 16:09:43 +0200

    add Greetng  exercise

---
+++ week-02/day-01/Greet.ts
@@ -0,0 +1,12 @@
+// -  Create variable named `nameToGreet` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `nameToGreet`
+
+let nameToGreet: string = 'Greenfox';
+function greet (greets: string) { 
+    console.log('Greetings, dear ', greets); }
+greet (nameToGreet);
+
+
