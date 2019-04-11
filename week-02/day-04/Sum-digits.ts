fa83c7a3a6f8fc86a297724652788cd32770c38b
meskom <mihaly.mesko@gmail.com>
2019-04-12 00:11:20 +0200

    add sum digits recursive exercise

--- week-02/day-04/Sum-digits.ts
+++ week-02/day-04/Sum-digits.ts
@@ -3,6 +3,14 @@
 //Given a non-negative int n, return the sum of its digits recursively (no loops). 
 //Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), 
 //while divide (/) by 10 removes the rightmost digit 
-//(126 / 10 is 12).
+//(126 / 10 is 12)
 
-console.log ((126/10)%10)
+function sumDigits(n:number):number { 
+    if (n == 0){
+        return 0;
+    }  
+    else return (n % 10) + sumDigits (Math.floor(n/10));   
+    }
+     
+
+console.log (sumDigits (155))
