9ab2be8f93ee910c98f8675c5d546d2be51b2396
meskom <mihaly.mesko@gmail.com>
2019-04-20 22:16:21 +0200

    add double-item exercise

---
+++ week-02/day-01/double-items.ts
@@ -0,0 +1,8 @@
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']
+
+let drink: string [] = ['Gin', 'Whiskey', 'Wine', 'Beer']
+let doubleDrink = drink.map(function (e) { return e + e})
+console.log(doubleDrink)
+
