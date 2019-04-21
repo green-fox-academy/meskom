228cc959dbb9f5e700bd9ab5f59dd08bca87f5f5
meskom <mihaly.mesko@gmail.com>
2019-04-21 12:15:29 +0200

    add subint exercise

---
+++ week-02/day-01/subint.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+//import { join } from "path";
+
+//  Create a function that takes a number and an array of numbers as a parameter
+//  Returns the indices of the numbers in the array of which the first number is a part of
+//  Or returns an empty list if the number is not part of any of the numbers in the array
+
+function subint(num: number, arr: number[]) {
+    let arr1: any[] = []
+    if (arr.join('').indexOf(num.toString()) == -1) {
+        return []
+    }
+    else {
+        for (let i: number = 0; i <= arr.join('').length; i++) {
+            
+            if (arr.join('')[i] == num.toString()) {
+                arr1.push(i)
+            }
+        }
+    }
+    return arr1
+}
+
+// Example
+console.log(subint(1, [1, 11, 34, 52, 61]));
+// should print: `[0, 1, 4]`
+console.log(subint(9, [1, 11, 34, 52, 61]));
+// should print: '[]'
