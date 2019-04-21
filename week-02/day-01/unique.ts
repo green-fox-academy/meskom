7c552cd4da8dd5e5136e535428d11a0b3631e75d
meskom <mihaly.mesko@gmail.com>
2019-04-21 10:23:06 +0200

    refact(delete lines)

--- week-02/day-01/unique.ts
+++ week-02/day-01/unique.ts
@@ -1,17 +1,14 @@
 //  Create a function that takes a list of numbers as a parameter
 //  Returns a list of numbers where every number in the list occurs only once
 
-function unique(arr:number[]) {
-    let arr1: number [] = arr.sort()
-        for (let i: number = 0; i < arr.length; i++){
-        
-        if (arr1[i] == arr1[i+1])
-        {
-         arr1.splice(i,1)  
-        } 
-    } return arr1
-          
+function unique(arr: number[]) {
+    arr = arr.sort()
+    for (let i: number = 0; i < arr.length; i++) {
 
+        if (arr[i] == arr[i + 1]) {
+            arr.splice(i, 1)
+        }
+    } return arr
 }
 
 //  Example
