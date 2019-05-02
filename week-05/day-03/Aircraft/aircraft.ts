c0fa28793cdab702ce5c1000300d1e1761f19f93
meskom <mihaly.mesko@gmail.com>
2019-05-02 16:17:37 +0200

    refact (add carrier methods)

--- week-05/day-03/Aircraft/aircraft.ts
+++ week-05/day-03/Aircraft/aircraft.ts
@@ -11,19 +11,19 @@ export class Aircraft {
         this.type = type;
         this.maxAmmo = maxAmmo;
         this.baseDamage = baseDamage;
-        this.ammo = ammo;
+        this.ammo = 0;
         //this.damage = damage
     }
     fight(): number {
-        this.ammo = 0;
         let damage: number = this.ammo * this.baseDamage;
+        this.ammo = 0;
         return damage;
     }
     refill(num: number): number {
         if (num > this.maxAmmo) {
             let fillAmmo: number = this.maxAmmo - this.ammo
             this.ammo = this.maxAmmo;
-            return num - fillAmmo;
+            return (num - fillAmmo);
         }
         this.ammo += num;
         return 0;
@@ -32,13 +32,13 @@ export class Aircraft {
         return this.type
     }
     getStatus():string{
-        return ` Type: ${this.type}, Ammo: ${this.ammo}, Base Damage ${this.baseDamage}, All Damage: ${this.baseDamage * this.ammo}`
+        return ` Type: ${this.type}, Ammo: ${this.ammo}, Base Damage ${this.baseDamage}, All Damage: ${this.baseDamage * this.ammo} \n`
     }
     isPriority(): boolean {
         return (this.type == 'F35')
     }
 }
-let F16: Aircraft = new Aircraft('F16', 8, 30, 8);
+let F16: Aircraft = new Aircraft('F16', 8, 30);
 let F35: Aircraft = new Aircraft('F35', 12, 50);
 
 // console.log(F16.isPriority());
--- week-05/day-03/Aircraft/carrier.ts
+++ week-05/day-03/Aircraft/carrier.ts
@@ -33,33 +33,59 @@ class AircraftCarrier {
             else secPrioraircrafts.push(this.aircrafts[i])
 
         }
-        
+
         if (allAmmo > this.ammoStore) {
             firstPriorAircrafts.forEach(plane => {
-                plane.refill(this.ammoStore);
+                this.ammoStore = plane.refill(this.ammoStore);
             });
             secPrioraircrafts.forEach(plane => {
-                plane.refill(this.ammoStore);
+                this.ammoStore = plane.refill(this.ammoStore);
             })
-            return 'not enough ammo'
+            return `not enough ammo, remain: ${this.ammoStore}`
         }
         this.aircrafts.forEach(plane => {
-            plane.refill(this.ammoStore)
+            this.ammoStore = plane.refill(this.ammoStore)
         })
-        return 'every craft full of ammo'
+        return `every craft full of ammo, remain: ${this.ammoStore}`
+    }
+    fight(enemy: AircraftCarrier) {
+        let allDamage: number = 0;
+        for (let i: number = 0; i < this.aircrafts.length; i++) {
+            this.aircrafts[i].fight();
+            allDamage += this.aircrafts[i].fight();
+        }
+        enemy.healthPoint -= allDamage
+
+
+    }
+    getSumDamage(): number {
+        let sumDamage = 0;
+        this.aircrafts.forEach(plane => {
+            sumDamage += plane.baseDamage * plane.ammo
+        });
+        return sumDamage
+
     }
 
-    fight() {
-
+    getStatus(): string {
+        let allStatus: string = '';
+        this.aircrafts.forEach(plane => {
+            allStatus += plane.getStatus()
+        });
+        return `HP: ${this.healthPoint}, Aircraft count: ${this.aircrafts.length}, Ammo Storage: ${this.ammoStore}, Total damage: ${this.getSumDamage()}
+    Aircrafts: \n ${allStatus}`
     }
-    getStatus() {
-
-    }
-
-
 }
-let F16: Aircraft = new Aircraft('F16', 8, 30, 8);
-let F35: Aircraft = new Aircraft('F35', 12, 50);
-let newCarrier = new AircraftCarrier([F16,F35])
+// Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
+// Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
+// Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
+// Type F16, Ammo: 8, Base Damage: 30, All Damage: 240
+// Type F16, Ammo: 8, Base Damage: 30, All Damage: 240
 
+let F16: Aircraft = new Aircraft('F16', 8, 30);
+let F35: Aircraft = new Aircraft('F35', 12, 50);
+let newCarrier = new AircraftCarrier([F16, F35]);
+
+//console.log(newCarrier.fill()) ;
 console.log(newCarrier.fill())
+console.log(newCarrier.getStatus());
