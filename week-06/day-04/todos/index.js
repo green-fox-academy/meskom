40b7f787aded93d1129560f291e9cda0d358ee4c
meskom <mihaly.mesko@gmail.com>
2019-05-17 15:21:27 +0200

    add to do exercise

---
+++ week-06/day-04/todos/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+var todos = ["get up", "survive","go back to bed"];
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  
+  res.render('home', {todos:todos});
+}
+);
+
+
+app.listen(PORT, () => {
+console.log(`Listening on port ${PORT}`); });
