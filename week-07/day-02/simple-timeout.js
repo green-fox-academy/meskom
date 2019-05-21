8c4689128a04de8e2846ae9616754a2c6e286338
meskom <mihaly.mesko@gmail.com>
2019-05-21 17:00:22 +0200

    add Simple timeoust task

---
+++ week-07/day-02/simple-timeout.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Write a program that prints apple after 3 seconds.
+
+setTimeout(() => {
+    console.log('apple'); // prints after one second
+  }, 1000);
+  console.log('pear'); // prints first
