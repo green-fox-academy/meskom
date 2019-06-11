33eaa875c4d5740d15b46c001a034eae67bb46ea
meskom <mihaly.mesko@gmail.com>
2019-06-12 00:46:13 +0200

    add bookstore task

---
+++ week-10/practice/bookstore/server.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'bookinfo'
+});
+
+app.use(express.json());
+
+// app.get('/title', (req, res) => {
+//     conn.query(`SELECT book_name FROM book_mast`, (err, row) => {
+//         res.send(row)
+//     })
+// })
+
+app.get('/allinfos', (req, res) => {
+    conn.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast
+    INNER JOIN author ON book_mast.aut_id = author.aut_id
+    INNER JOIN category ON book_mast.cate_id = category.cate_id
+    INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;`,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Sql error!');
+            } else {
+                console.log(result)
+                res.send(result);
+            }
+        });
+});
+//res.sendFile(path.join(__dirname, 'public/index.html'));
+
+app.listen(port, () => console.log(`listening on port ${port}`))
