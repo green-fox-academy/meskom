ed595db97cee4370f82948fc0aa8299a5bbefeb5
meskom <mihaly.mesko@gmail.com>
2019-06-13 21:00:06 +0200

    add filter options on BE

--- week-10/practice/bookstore/server.js
+++ week-10/practice/bookstore/server.js
@@ -16,18 +16,37 @@ let conn = mysql.createConnection({
 
 app.use(express.json());
 
-// app.get('/title', (req, res) => {
-//     conn.query(`SELECT book_name FROM book_mast`, (err, row) => {
-//         res.send(row)
-//     })
-// })
+app.get('/title', (req, res) => {
+    conn.query(`SELECT book_name FROM book_mast`, (err, row) => {
+        res.send(row)
+    })
+})
 
+// app.get('/books', (req, res) => {
+//     conn.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast
+//     INNER JOIN author ON book_mast.aut_id = author.aut_id
+//     INNER JOIN category ON book_mast.cate_id = category.cate_id
+//     INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;`,
+//         (err, result) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.status(500).send('Sql error!');
+//             } else {
+//                 console.log(result)
+//                 res.status(200).send(result);
+//             }
+//         });
+// });
 app.get('/books', (req, res) => {
-    conn.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast
+    let query = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast
     INNER JOIN author ON book_mast.aut_id = author.aut_id
     INNER JOIN category ON book_mast.cate_id = category.cate_id
-    INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;`,
-        (err, result) => {
+    INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id `
+    if (req.query.category) {
+        query += `WHERE cate_descrip = ${req.query.category}`
+
+        conn.query((query), (err, result) => {
+
             if (err) {
                 console.log(err);
                 res.status(500).send('Sql error!');
@@ -36,9 +55,58 @@ app.get('/books', (req, res) => {
                 res.status(200).send(result);
             }
         });
-});
-app.get('/test', (req, res) => {
-res.sendFile(path.join(__dirname, 'public/index.html'));
+    }
+    if (req.query.publisher) {
+        let pub = req.query.publisher
+        query += `WHERE pub_name LIKE "%${pub}%"`;
+
+        conn.query((query), (err, result) => {
+
+            if (err) {
+                console.log(err);
+                res.status(500).send('Sql error!');
+            } else {
+                console.log(result)
+                res.status(200).send(result);
+            }
+        });
+    }
+    if (req.query.plt) {
+        query += `WHERE book_price < ${req.query.plt}`
+
+        conn.query((query), (err, result) => {
+
+            if (err) {
+                console.log(err);
+                res.status(500).send('Sql error!');
+            } else {
+                console.log(result)
+                res.status(200).send(result);
+            }
+        });
+    }
+    if (req.query.pgt) {
+        query += `WHERE book_price > ${req.query.pgt}`
+
+        conn.query((query), (err, result) => {
+
+            if (err) {
+                console.log(err);
+                res.status(500).send('Sql error!');
+            } else {
+                console.log(result)
+                res.status(200).send(result);
+            }
+        });
+    }
 });
 
+
+
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
 app.listen(port, () => console.log(`listening on port ${port}`))
+
